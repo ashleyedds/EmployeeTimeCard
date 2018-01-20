dd30b62995fb699f7258a80c0b2fef8b1a96ca40
Tony Gottschalk <tony.gottschalk2017@gmail.com>
2018-01-20 10:53:13 -0500

    pushing JS update
    var

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,9 +10,44 @@
 
 firebase.initializeApp(config);
 
+var employeeName = "";
+var role = "";
+var startDate = "";
+var monthsWorked = "";
+var monthlyRate = "";
+var totalBilled = "";
+
+$('#add-user').on('click', function() {
+	event.preventDefault();
+
+	employeeName = $('#name-input').val().trim();
+	role = $('#email-input').val().trim();
+	startDate = $('#age-input').val().trim();
+	monthsWorked = $('#comment-input').val().trim();
+	monthlyRate = $('#comment-input').val().trim();
+	totalBilled = $('#comment-input').val().trim();
 
 
+//.set replaces old data with new data but does not add on
+	firebase.database().ref().push( {
+	// firebase.database().ref().set( {
+		name: name,
+		email: email,
+		age: age,
+		comment: comment,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+});
 
+//snapshot is response we are getting from the server
+firebase.database().ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+// firebase.database().ref().on("value", function(snapshot) {
+	$("#name-display").html(snapshot.val().name);
+	$("#email-display").html(snapshot.val().email);
+	$("#age-display").html(snapshot.val().age);
+	$("#comment-display").html(snapshot.val().comment);
+
+});
 
 
 
